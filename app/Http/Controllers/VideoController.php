883fe4181ad1da2cdc576b96cb0c7e57becf8346
jennifer <jennifer@localhost.localdomain>
2019-07-10 06:29:25 +0000

    oss文件上传

---
+++ app/Http/Controllers/VideoController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\VideoModel;
+use Illuminate\Http\Request;
+use OSS\OssClient;
+
+class VideoController extends Controller
+{
+    protected $accessKeyID = 'LTAIH1Tlj5uO2Ysh';
+    protected $accessKeySecret = '2iusAO0v5zS9Q9kBr81OIRz0HwvV8u';
+    protected $bucket = 'video-1809a';
+
+    //图片上传
+    public function oss_upload(){
+        $client = new OssClient($this->accessKeyID,$this->accessKeySecret,env('ALI_OSS_ENDPOINT'));
+        $obj = md5(time().mt_rand(1,99999)).'jpg'; //文件名
+        $local_file = 'p1.jpg';
+        $res = $client->uploadFile($this->bucket,$obj,$local_file);
+        dump($res);
+    }
+
+    //文件上传
+    public function oss_file(){
+        $client = new OssClient($this->accessKeyID,$this->accessKeySecret,env('ALI_OSS_ENDPOINT'));
+        $obj = "hello"; //文件名
+        $cont = 'hello everyone';
+        $res = $client->putObject($this->bucket,$obj,$cont);
+        dump($res);
+    }
+}
